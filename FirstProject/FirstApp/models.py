53dc9cf9b5fe04b255b54f34bbe09586163a3742
PVPSMILE <88920617+PVPSMILE@users.noreply.github.com>
2022-05-21 21:42:59 +0300

    + tempaltes new_url ""

--- FirstProject/FirstApp/models.py
+++ FirstProject/FirstApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    raiting = models.CharField(max_length=255)
+    description = models.TextField(blank= True) # blank = True - данное поле может быть пустым
+    price = models.CharField(max_length=255)
+    img_url = models.CharField(max_length=255)
--- FirstProject/FirstProject/settings.py
+++ FirstProject/FirstProject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FirstAppapps.appsFirstappConfig'
+    'FirstApp.apps.FirstappConfig'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'FirstProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Python\logika\Web\DjangoProject\FirstProject\FirstApp\teplates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
